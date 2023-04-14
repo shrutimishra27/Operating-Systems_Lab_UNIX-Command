ed7622f7871ace8a06c8aa922f7d29c048722a15
Shruti Mishra <96042938+Shruti-Mishra27@users.noreply.github.com>
2023-04-14 19:56:47 +0530

    Processes

---
+++ 4/program1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+	fork();
+	printf("Hello World\n");
+	return 0;
+}
---
+++ 4/program2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+	fork();
+	fork();
+	fork();
+	printf("hello\n");
+	return 0;
+}
